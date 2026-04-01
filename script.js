6f42a563227571211a6f8b7988f8142af3aba37e
mlodySajmon <Szymiwziatek11@gmail.com>
2026-04-01 07:29:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+//Ciekawostki
+
+function pokazCiekawostke() {
+    const ciekawostki = [
+        "Zermatt jest miastem bez samochodów porusza się tu głównie pojazdami elektrycznymi.",
+        "Matterhorn ma 4478 m wysokości.",
+        "Zermatt to jeden z najpopularniejszych kurortów narciarskich w Europie."
+    ];
+
+    const losowa = Math.floor(Math.random() * ciekawostki.length);
+    document.getElementById("ciekawostka").innerText = ciekawostki[losowa];
+}
+
+// Tablica zdjęć
+let zdjecia = [
+    "Obrazy/1.jpg",
+    "Obrazy/2.jpg",
+    "Obrazy/3.jpg",
+    "Obrazy/4.jpg",
+    "Obrazy/5.jpg",
+    "Obrazy/baner1.jpg",
+];
+
+// Aktualny numer zdjęcia
+let numer = 0;
+
+// Funkcja następnego zdjęcia
+function nastepne(){
+    numer = (numer + 1) % zdjecia.length;
+    document.getElementById("slider").src = zdjecia[numer];
+}
+
+// Funkcja poprzedniego zdjęcia
+function poprzednie(){
+    numer = (numer - 1 + zdjecia.length) % zdjecia.length;
+    document.getElementById("slider").src = zdjecia[numer];
+}
+//Quiz
+
+let punkty = 0;
+
+function sprawdzOdpowiedz(nr) {
+
+    if (nr === 2 || nr === 4) {
+        punkty++;
+    }
+
+    document.getElementById("wynik").innerText =
+        "Twój wynik: " + punkty + " punkty";
+}
